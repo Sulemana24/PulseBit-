a8d5c7e9b35c2aa02553f0770f462fe1d52463b0
Iddrisu_Sulemana <iddrisusulemana665@gmail.com>
2024-11-14 15:54:56 +0000

    files are now separated

--- home.js
+++ home.js
@@ -13,3 +13,35 @@ navLinks.addEventListener("click", (e) => {
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
+
+// Toggle dark mode on click
+
+const darkModeToggle = document.getElementById('darkModeToggle');
+
+
+if (localStorage.getItem('dark-mode') === 'enabled') {
+    document.body.classList.add('dark-mode');
+    darkModeToggle.classList.add('dark-active');
+}
+
+
+
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    darkModeToggle.classList.toggle('dark-active'); 
+
+
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('dark-mode', 'enabled');
+    } else {
+        localStorage.setItem('dark-mode', 'disabled');
+    }
+});
+
+window.onscroll = function() { scrollFunction() };
+function scrollFunction() {
+    document.getElementById("backToTop").style.display = document.documentElement.scrollTop > 20 ? "block" : "none";
+}
+function topFunction() {
+    document.documentElement.scrollTop = 0;
+}
