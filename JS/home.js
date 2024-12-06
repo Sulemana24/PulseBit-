db8a4e7d80e39190d65ea239b21f6ab22198c630
Iddrisu_Sulemana <iddrisusulemana665@gmail.com>
2024-12-06 11:57:53 +0000

    megged articles files into one

--- JS/home.js
+++ JS/home.js
@@ -121,3 +121,10 @@
         popup.style.display = 'none';
     }
     });
+
+    const currentURL = window.location.href;
+
+
+    document.querySelector('.share-btn-facebook').href = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(currentURL)}`;
+    document.querySelector('.share-btn-twitter').href = `https://twitter.com/intent/tweet?url=${encodeURIComponent(currentURL)}&text=Check%20out%20this%20post!`;
+    document.querySelector('.share-btn-linkedin').href = `https://www.linkedin.com/shareArticle?url=${encodeURIComponent(currentURL)}&title=Check%20out%20this%20post!`;
